b93988de575fe62850c7822a2f6840bf9a38bd65
Айрат Курбанов <KurbanovAyR@stud.kai.ru>
2024-05-01 19:11:00 +0300

    доделал старт и сделал страничку ожидание

--- gridd/Controllers/HomeController.cs
+++ gridd/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace gridd.Controllers
             return View();
         }
 
+        public IActionResult WaitPage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
